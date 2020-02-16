74ace2bf278fe7a637c394d7408748e82511496e
mkeoliya <keoliyamayank@gmail.com>
2020-02-16 12:02:29 +0800

    Removed redundant files, created folders.

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 <style type="text/css">
     body,td,th {
 	color: #FFFFFF;
-	
+
 }
 .styled select {
 	-webkit-appearance: button;
@@ -35,14 +35,14 @@
   margin: 5px 1px 3px 0px;
   border: 1px solid #DDDDDD;
 }
- 
+
 input[type=text]:focus, textarea:focus {
   box-shadow: 0 0 5px rgba(81, 203, 238, 1);
   padding: 3px 0px 3px 3px;
   margin: 5px 1px 3px 0px;
   border: 1px solid rgba(81, 203, 238, 1);
 }
-	
+
   background: transparent;
   width: 150px;
   font-size: 20px;
@@ -56,15 +56,16 @@ input[type=text]:focus, textarea:focus {
   border: 1px solid #111;
   border-radius: 3px;
   overflow: hidden;
-  
+
 }
 </style>
 </head>
 
 <body>
 <?php
-include("header1.php");
-include("header.php");
+include("header-files/header1.php");
+include("header-files/header.php");
+include("header-files/conn.php");
 if(isset($_POST['submit1']))
 {
 	header('Location: pointsHome.php');
